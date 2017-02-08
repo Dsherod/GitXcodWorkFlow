2f3460f44c17101b2c0fb11844f7e3c976c33a7a
DeVante Sherod <devantesherod@mwc-009142.dhcp.missouri.edu>
2017-02-08 12:41:39 -0600

    Created an Xcode project

---
+++ GitXcodeWorkFlow/GitXcodeWorkFlow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitXcodeWorkFlow
+//
+//  Created by DeVante Sherod on 2/8/17.
+//  Copyright © 2017 DeVante Sherod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
